deb64deb4f2dbd2b702dfd422eb371679cac00eb
Unlight-FOX <61931895+Unlight-FOX@users.noreply.github.com>
2020-03-20 20:18:26 +0100

    Podstawowy projekt
    Stworzony podstawowy zestaw plików projektu
    + okno bez ramki 
    + dwa przyciski: 
       > close_button (z dodaną funkcją zamykania aplikacji)
       > play_button (bez ewentów)
    + panel pozwalający na przesuwanie okna.

---
+++ project-0.1/Player/Player/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.close_button = new System.Windows.Forms.Button();
+            this.play_button = new System.Windows.Forms.Button();
+            this.drag_panel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.drag_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.play_button);
+            this.panel1.Location = new System.Drawing.Point(12, 28);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1277, 491);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // close_button
+            // 
+            this.close_button.BackColor = System.Drawing.Color.Black;
+            this.close_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.close_button.FlatAppearance.BorderSize = 0;
+            this.close_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close_button.ForeColor = System.Drawing.Color.Transparent;
+            this.close_button.Image = global::Player.Properties.Resources.close;
+            this.close_button.Location = new System.Drawing.Point(1249, 3);
+            this.close_button.Margin = new System.Windows.Forms.Padding(0);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(40, 40);
+            this.close_button.TabIndex = 2;
+            this.close_button.UseVisualStyleBackColor = false;
+            this.close_button.Click += new System.EventHandler(this.close_button_Click);
+            // 
+            // play_button
+            // 
+            this.play_button.BackColor = System.Drawing.Color.Black;
+            this.play_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.play_button.FlatAppearance.BorderSize = 0;
+            this.play_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.play_button.ForeColor = System.Drawing.Color.Transparent;
+            this.play_button.Image = global::Player.Properties.Resources.play1;
+            this.play_button.Location = new System.Drawing.Point(60, 431);
+            this.play_button.Margin = new System.Windows.Forms.Padding(0);
+            this.play_button.Name = "play_button";
+            this.play_button.Size = new System.Drawing.Size(60, 60);
+            this.play_button.TabIndex = 1;
+            this.play_button.UseVisualStyleBackColor = false;
+            this.play_button.Click += new System.EventHandler(this.play_button_Click);
+            // 
+            // drag_panel
+            // 
+            this.drag_panel.Controls.Add(this.close_button);
+            this.drag_panel.Controls.Add(this.panel1);
+            this.drag_panel.Location = new System.Drawing.Point(3, 6);
+            this.drag_panel.Name = "drag_panel";
+            this.drag_panel.Size = new System.Drawing.Size(1296, 533);
+            this.drag_panel.TabIndex = 3;
+            this.drag_panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drag_panel_MouseDown);
+            this.drag_panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drag_panel_MouseMove);
+            this.drag_panel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drag_panel_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1301, 534);
+            this.Controls.Add(this.drag_panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.drag_panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button play_button;
+        private System.Windows.Forms.Button close_button;
+        private System.Windows.Forms.Panel drag_panel;
+    }
+}
+
---
+++ project-0.1/Player/Player/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void play_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void close_button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        int mouseX = 0;
+        int mouseY = 0;
+        bool mouseDown;
+
+        private void drag_panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+        }
+
+        private void drag_panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(mouseDown==true)
+            {
+                mouseX = MousePosition.X - 500;
+                mouseY = MousePosition.Y-20;
+
+                this.SetDesktopLocation(mouseX, mouseY);
+            }
+        }
+
+        private void drag_panel_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+    }
+}
