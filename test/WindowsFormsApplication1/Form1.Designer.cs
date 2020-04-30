4c90fe787423ec6841b728e2cd9c8ba7778f54d9
michp97 <62289796+michp97@users.noreply.github.com>
2020-04-30 13:34:59 +0200

    Add files via upload
    kod źródłowy testowego builda, dodano naprawiony pasek postępu oraz przycisk stop i play/pause

---
+++ test/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.vdoTrackBar = new System.Windows.Forms.TrackBar();
+            this.VolumeTrackBar = new System.Windows.Forms.TrackBar();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.vdoTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vdoTrackBar
+            // 
+            this.vdoTrackBar.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.vdoTrackBar.Location = new System.Drawing.Point(11, 444);
+            this.vdoTrackBar.Margin = new System.Windows.Forms.Padding(2);
+            this.vdoTrackBar.Name = "vdoTrackBar";
+            this.vdoTrackBar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.vdoTrackBar.Size = new System.Drawing.Size(584, 45);
+            this.vdoTrackBar.TabIndex = 0;
+            this.vdoTrackBar.TabStop = false;
+            this.vdoTrackBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.vdoTrackBar.Scroll += new System.EventHandler(this.vdoTrackBar_Scroll_1);
+            // 
+            // VolumeTrackBar
+            // 
+            this.VolumeTrackBar.Location = new System.Drawing.Point(511, 495);
+            this.VolumeTrackBar.Margin = new System.Windows.Forms.Padding(2);
+            this.VolumeTrackBar.Name = "VolumeTrackBar";
+            this.VolumeTrackBar.Size = new System.Drawing.Size(84, 45);
+            this.VolumeTrackBar.TabIndex = 2;
+            this.VolumeTrackBar.TabStop = false;
+            this.VolumeTrackBar.Scroll += new System.EventHandler(this.VolumeTrackBar_Scroll);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(11, 493);
+            this.loadButton.Margin = new System.Windows.Forms.Padding(2);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(56, 47);
+            this.loadButton.TabIndex = 3;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(10, 11);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(585, 375);
+            this.panel1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(72, 516);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(40, 23);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(118, 516);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(39, 23);
+            this.playButton.TabIndex = 7;
+            this.playButton.Text = "play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(608, 551);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.VolumeTrackBar);
+            this.Controls.Add(this.vdoTrackBar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.vdoTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar vdoTrackBar;
+        private System.Windows.Forms.TrackBar VolumeTrackBar;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button playButton;
+    }
+}
+
---
+++ test/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.DirectX.AudioVideoPlayback;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Video vdo;
+        public string mode = "play";
+        public string PlayingPosition, Duration;
+
+        public Form1()
+        {
+            InitializeComponent();
+            VolumeTrackBar.Value = 4;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            if (vdo != null)
+            {
+                vdo.Stop();
+            }
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            openFileDialog.Title = "Select video file..";
+            openFileDialog.InitialDirectory = Application.StartupPath;
+            openFileDialog.DefaultExt = ".avi";
+            openFileDialog.Filter = "Media Files|*.mpg;*.avi;*.wma;*.mov;*.wav;*.mp2;*.mp3|All Files|*.*";
+            if (openFileDialog.FileName != "")
+            {
+                Form1.ActiveForm.Text = openFileDialog.FileName + " - Victure";
+                vdo = new Video(openFileDialog.FileName);
+
+                vdo.Owner = panel1;
+                vdoTrackBar.Maximum = Convert.ToInt32(vdo.Duration);
+                panel1.Width = 500;
+                panel1.Height = 300;
+                vdo.Play();
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            //PlayingPosition = CalculateTime(vdo.CurrentPosition);
+            ////txtStatus.Text = PlayingPosition + "/" + Duration;
+
+            //if (vdo.CurrentPosition >= vdo.Duration)
+            //{
+            //    timer1.Stop();
+            //    Duration = CalculateTime(vdo.Duration);
+            //    PlayingPosition = "0:00:00";
+            //    //txtStatus.Text = PlayingPosition + "/" + Duration;
+            //    vdo.Stop();
+            //    vdoTrackBar.Value = 0;
+            //}
+            //else
+            //    vdoTrackBar.Value += 1;
+
+            if (vdo!=null){
+                vdoTrackBar.Value = Convert.ToInt32(vdo.CurrentPosition);
+            }
+        }
+
+        public string CalculateTime(double Time)
+        {
+            string mm, ss, CalculatedTime;
+            int h, m, s, T;
+
+            Time = Math.Round(Time);
+            T = Convert.ToInt32(Time);
+
+            h = (T / 3600);
+            T = T % 3600;
+            m = (T / 60);
+            s = T % 60;
+
+            if (m < 10)
+                mm = string.Format("0{0}", m);
+            else
+                mm = m.ToString();
+            if (s < 10)
+                ss = string.Format("0{0}", s);
+            else
+                ss = s.ToString();
+
+            CalculatedTime = string.Format("{0}:{1}:{2}", h, mm, ss);
+
+            return CalculatedTime;
+        }
+
+        private void VolumeTrackBar_Scroll(object sender, EventArgs e)
+        {
+            if (vdo != null)
+            {
+                vdo.Audio.Volume = CalculateVolume();
+            }
+        }
+        public int CalculateVolume()
+        {
+            switch (VolumeTrackBar.Value)
+            {
+                case 1:
+                    return -1500;
+                case 2:
+                    return -1000;
+                case 3:
+                    return -700;
+                case 4:
+                    return -600;
+                case 5:
+                    return -500;
+                case 6:
+                    return -400;
+                case 7:
+                    return -300;
+                case 8:
+                    return -200;
+                case 9:
+                    return -100;
+                case 10:
+                    return 0;
+                default:
+                    return -10000;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Duration = CalculateTime(vdo.Duration);
+            PlayingPosition = "0:00:00";
+            //txtStatus.Text = PlayingPosition + "/" + Duration;
+        }
+
+        private void VolumeTrackBar_Scroll_1(object sender, EventArgs e)
+        {
+            if (vdo != null)
+            {
+                vdo.Audio.Volume = CalculateVolume();
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (vdo != null)
+            {
+                vdo.Stop();
+                vdoTrackBar.ResetCursor();
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (vdo != null)
+            {
+                StateFlags status = vdo.State;
+                switch (status)
+                {
+                    case StateFlags.Stopped:
+                        vdo.Play();
+                        break;
+                    case StateFlags.Paused:
+                        vdo.Play();
+                        break;
+                    case StateFlags.Running:
+                        vdo.Pause();
+                        break;
+                }
+
+            }
+        }
+
+        private void vdoTrackBar_Scroll_1(object sender, EventArgs e)
+        {
+            if (vdo != null)
+            {
+                vdo.CurrentPosition = vdoTrackBar.Value;
+            }
+        }
+    }
+}
