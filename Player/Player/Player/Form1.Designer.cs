c7d9c7d8b5123a16db97e8eb8bb631fafd9e1337
michp97 <62289796+michp97@users.noreply.github.com>
2020-05-02 02:35:06 +0200

    Add files via upload

--- Player/Player/Player/Form1.Designer.cs
+++ Player/Player/Player/Form1.Designer.cs
@@ -31,17 +31,27 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.openButton = new System.Windows.Forms.Button();
             this.playButton = new System.Windows.Forms.Button();
-            this.buttonClose = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.progressBar = new System.Windows.Forms.TrackBar();
+            this.volumeBar = new System.Windows.Forms.TrackBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel1.Controls.Add(this.buttonClose);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(0, 26);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1317, 477);
+            this.panel1.Size = new System.Drawing.Size(1006, 405);
             this.panel1.TabIndex = 0;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
@@ -49,50 +59,131 @@
             // 
             // openButton
             // 
-            this.openButton.Location = new System.Drawing.Point(1254, 507);
+            this.openButton.Location = new System.Drawing.Point(108, 486);
+            this.openButton.Margin = new System.Windows.Forms.Padding(2);
             this.openButton.Name = "openButton";
-            this.openButton.Size = new System.Drawing.Size(75, 46);
+            this.openButton.Size = new System.Drawing.Size(44, 25);
             this.openButton.TabIndex = 1;
-            this.openButton.Text = "button1";
+            this.openButton.Text = "load";
             this.openButton.UseVisualStyleBackColor = true;
             this.openButton.Click += new System.EventHandler(this.openButton_Click);
             // 
             // playButton
             // 
             this.playButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.playButton.Location = new System.Drawing.Point(12, 496);
+            this.playButton.Location = new System.Drawing.Point(11, 486);
+            this.playButton.Margin = new System.Windows.Forms.Padding(2);
             this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(75, 57);
+            this.playButton.Size = new System.Drawing.Size(44, 25);
             this.playButton.TabIndex = 2;
             this.playButton.Text = " Play";
             this.playButton.UseVisualStyleBackColor = true;
             this.playButton.Click += new System.EventHandler(this.playButton_Click);
             // 
-            // buttonClose
+            // progressBar
             // 
-            this.buttonClose.Location = new System.Drawing.Point(1236, 0);
-            this.buttonClose.Name = "buttonClose";
-            this.buttonClose.Size = new System.Drawing.Size(78, 46);
-            this.buttonClose.TabIndex = 3;
-            this.buttonClose.Text = "CLOSE";
-            this.buttonClose.UseVisualStyleBackColor = true;
-            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            this.progressBar.Location = new System.Drawing.Point(0, 436);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1006, 45);
+            this.progressBar.TabIndex = 4;
+            this.progressBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            // 
+            // volumeBar
+            // 
+            this.volumeBar.Location = new System.Drawing.Point(902, 486);
+            this.volumeBar.Name = "volumeBar";
+            this.volumeBar.Size = new System.Drawing.Size(104, 45);
+            this.volumeBar.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DarkGray;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1006, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opemToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // opemToolStripMenuItem
+            // 
+            this.opemToolStripMenuItem.Name = "opemToolStripMenuItem";
+            this.opemToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.opemToolStripMenuItem.Text = "Open";
+            this.opemToolStripMenuItem.Click += new System.EventHandler(this.opemToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(8, 539);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(79, 539);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(60, 486);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(43, 25);
+            this.stopButton.TabIndex = 0;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(1341, 565);
-            this.Controls.Add(this.playButton);
+            this.ClientSize = new System.Drawing.Size(1006, 561);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.openButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.volumeBar);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.playButton);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.progressBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,7 +192,15 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button openButton;
         private System.Windows.Forms.Button playButton;
-        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TrackBar progressBar;
+        private System.Windows.Forms.TrackBar volumeBar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button stopButton;
     }
 }
 
